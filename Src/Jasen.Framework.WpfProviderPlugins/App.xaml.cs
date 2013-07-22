b6dad5dfb621140edcd7b67e7ace3de18a585646
jasenkin <jasen.kin@gmail.com>
2013-07-22 23:53:11 +0800

    Add wpf project
    Add wpf project, just contains the theme of style .

---
+++ Src/Jasen.Framework.WpfProviderPlugins/App.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace Jasen.Framework.WpfProviderPlugins
+{
+    /// <summary>
+    /// App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App : Application
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            if (e.Exception is InvalidOperationException)
+                e.Handled = true;
+        }
+
+        /// <summary>
+        /// save application state and data.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Application_Exit(object sender, ExitEventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Load basic data
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Src/Jasen.Framework.WpfProviderPlugins/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Jasen.Framework.WpfProviderPlugins
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.Loaded += new RoutedEventHandler(MainWindow_Loaded);
+        }
+
+        void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.comboBox.ItemsSource = GetData();
+            this.listBox.ItemsSource = GetData();
+        }
+
+        public IList<Person> GetData()
+        {
+            return new List<Person>() { 
+            new Person() { Name = "Eric", Surname="Cartman" },
+            new Person() { Name = "Stan", Surname="Marsh" },
+            new Person() { Name = "Kyle", Surname="Broflovski" },
+            new Person() { Name = "Kenny", Surname="McCormick" },
+            new Person() { Name = "Bebe", Surname="Stevens" },
+            new Person() { Name = "Clyde", Surname="Donovan" },  
+            new Person() { Name = "Annie", Surname="Polk" },
+            new Person() { Name = "Randy", Surname="Marsh" },
+            new Person() { Name = "Sharon", Surname="Marsh" }, 
+            new Person() { Name = "Liane", Surname="Cartman" },
+            new Person() { Name = "Stuart", Surname="McCormick" },
+            new Person() { Name = "Carol", Surname="McCormick" },
+            new Person() { Name = "Kevin", Surname="McCormick" },
+            new Person() { Name = "Stephen", Surname="Stotch" },
+            new Person() { Name = "Linda", Surname="Stotch" },
+            new Person() { Name = "Richard", Surname="Tweak" }
+            };
+        }
+       
+
+    }
+}
